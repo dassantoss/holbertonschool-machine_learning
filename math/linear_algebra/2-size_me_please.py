c17a5fbe8b7bbbdde5b6e447ede201be6a9b42f5
dassantoss <santy.e@gmail.com>
2024-04-12 13:06:55 -0600

    Update: Add documentation.

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -5,6 +5,15 @@ Determine the shape of a matrix as a list of its dimensions.
 
 
 def matrix_shape(matrix):
+    """
+    Determine the shape of a matrix as a list of its dimensions.
+    
+    Parameters:
+    - matrix: The input matrix for which the shape needs to be calculated.
+
+    Returns:
+    A list of integers representing the dimensions of the matrix.
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
