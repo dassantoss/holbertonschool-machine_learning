ac6f43a911c67b8eed99a93f745436186e93fcd7
dassantoss <santy.e@gmail.com>
2024-04-13 17:04:48 -0600

    Add Function that calculates the shape of a numpy.ndarray.

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Function that calculates the shape of a numpy.ndarray.
+"""
+import numpy as np
+
+
+def np_shape(matrix):
+    """
+    Calculate the shape of a numpy.ndaray.
+
+    Parameters:
+    matrix (numpy ndarray): The arrar for which to determine the shape.
+
+    Returns:
+    tuple: A tuple of integers representing the shape of the matrix.
+    """
+    return matrix.shape
