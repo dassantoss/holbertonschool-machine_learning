f8b216ba615996659f81596864d723f7075bca55
dassantoss <santy.e@gmail.com>
2024-04-12 19:00:48 -0600

    Add Function that concatenates two arrays.

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Function that concatenates two arrays.
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenates two lists of integers or floats.
+
+    Parameters:
+    arr1 (list of ints/floats): First list to concatenate.
+    arr2 (list of ints/floats): Second list to concatenate.
+
+    Returns:
+    list: A new list resulting from the concatenation of arr1 and arr2.
+    """
+
+    return arr1 + arr2
