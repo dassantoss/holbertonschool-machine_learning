ee3e35ed1441d223d9c837b1f9d6dade6aca5aa0
dassantoss <santy.e@gmail.com>
2024-04-12 20:53:53 -0600

    Fix

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -17,14 +17,24 @@ def cat_matrices2D(mat1, mat2, axis=0):
     list of list of ints/floats: New matrix resulting from concatenatenation.
     None: If the concatenation is not posible due to dimension mismatch.
     """
+    if not mat1 and not mat2:
+        return []
+    if not mat1:
+        return mat2[:] if axis == 0 else None
+    if not mat2:
+        return mat1[:] if axis == 0 else None
+
     if axis == 0:
         # Check if both matrices have the same number of columns
-        if not all(len(row) == len(mat1[0]) for row in mat1) or not all(len(row) == len(mat2[0]) for row in mat2):
+        if len(mat1[0]) == len(mat2[0]):
+            return mat1 + mat2
+        else:
             return None
-        return mat1 + mat2
     elif axis == 1:
         # Check if both matrices have the same number of rows
-        if len(mat1) != len(mat2) or not all(len(row1) == len(row2) for row1, row2 in zip(mat1, mat2)):
+        if len(mat1) == len(mat2):
+            return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
+        else:
             return None
-        return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
+
     return None
