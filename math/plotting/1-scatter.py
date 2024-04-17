68469d72d340e353849cd6c76a60566c34c2262f
dassantoss <santy.e@gmail.com>
2024-04-17 13:36:47 -0600

    Fix

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -23,14 +23,14 @@ def scatter():
     """
 
     mean = [69, 0]
-    cov = [[115, 8], [8, 15]]
+    cov = [[15, 8], [8, 15]]
     np.random.seed(5)
     x, y = np.random.multivariate_normal(mean, cov, 2000).T
     y += 180
     plt.figure(figsize=(6.4, 4.8))
 
     plt.title("Men's Height vs Weight")
-    plt.ylabel("Weight (lbs)")
     plt.xlabel("Height (in)")
+    plt.ylabel("Weight (lbs)")
     plt.scatter(x, y, color='m')
     plt.show()
