fd07e8c0c659a21a0be076dcdc0b371c34c4f163
dassantoss <santy.e@gmail.com>
2024-04-17 13:26:14 -0600

    Add scatter plot.

---
+++ math/plotting/1-scatter.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+This module creates a scatter plot representing men's height vs wight using
+a multivariate normal distribution for the data points.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def scatter():
+    """
+    Creates and display a scatter plot of men's height vs weight.
+
+    Parameters:
+    None
+
+    Returns:
+    None
+
+    Example:
+    >>> scatter()
+    # This will display the scatter plot in a matplotlib viewer.
+    """
+
+    mean = [69, 0]
+    cov = [[115, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.title("Men's Height vs Weight")
+    plt.ylabel("Weight (lbs)")
+    plt.xlabel("Height (in)")
+    plt.scatter(x, y, color='magenta')
+    plt.show()
