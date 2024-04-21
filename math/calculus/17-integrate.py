3e2c3ba4899bf5028bf6e0aed23114d0942d2a53
dassantoss <santy.e@gmail.com>
2024-04-21 00:14:30 -0600

    Fix output

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -19,14 +19,25 @@ def poly_integral(poly, C=0):
     polynomial.
     """
     # Check if input is valid
-    if not isinstance(poly, list) \
-        or not all(isinstance(c, int)
-                   for c in poly) or not isinstance(C, int):
+    if not isinstance(poly, list) or not all(isinstance(c, int)
+                                             for c in poly):
         return None
+
+    # If the polynomial is of 0 degree (constant), return C as the integral
+    if len(poly) == 0:
+        return [C]
+    if len(poly) == 1:
+        return [C, poly[0]]
 
     # Integrate each term and create a new list of coefficients
     integral = [C] + [coeff / (i + 1) for i, coeff in enumerate(poly)]
-    integral = [int(c) if isinstance(c, float) and c.is_integer()
-                else c for c in integral]
+
+    # Remove trailing zeros to minimize the length of the list
+    while integral[-1] == 0 and len(integral) > 1:
+        integral.pop()
+
+    # Convert floating point numbers to integers if they are whole numbers
+    integral = [int(c) if isinstance(c, float)
+                and c.is_integer() else c for c in integral]
 
     return integral
