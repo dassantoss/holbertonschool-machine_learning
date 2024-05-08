a89b9800321aee677d2c814fb2664b6f0c001227
dassantoss <santy.e@gmail.com>
2024-05-08 12:28:15 -0600

    Fix pycodestyle.

--- supervised_learning/classification/11-neural_network.py
+++ supervised_learning/classification/11-neural_network.py
@@ -111,5 +111,5 @@ class NeuralNetwork:
         """
         m = Y.shape[1]
         cost = -(1 / m) * np.sum(Y * np.log(A) + (1 - Y)
-                                  * np.log(1.0000001 - A))
+                                 * np.log(1.0000001 - A))
         return cost
