50611ef8b3f1ece3c38975df0b5493b0c53a0027
dassantoss <santy.e@gmail.com>
2024-05-11 12:55:53 -0600

    Fix

--- supervised_learning/tensorflow/1-main.py
+++ supervised_learning/tensorflow/1-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import tensorflow.compat.v1 as tf
+import tensorflow.compat.v1 as tf # type: ignore
 tf.disable_eager_execution()
 
 create_placeholders = __import__('0-create_placeholders').create_placeholders
