bfd2a07e08f7f99e6176e32c651ad5401277a21d
dassantoss <santy.e@gmail.com>
2024-06-02 17:35:55 -0600

    Add function thats Trains a model using mini-batch gradient descent.

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Trains a model using mini-batch gradient descent.
+"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs, verbose=True,
+                shuffle=False):
+    """
+    Trains a model using mini-batch gradient descent.
+
+    Args:
+        network (keras.Model): The model to train.
+        data (numpy.ndarray): Input data of shape (m, nx).
+        labels (numpy.ndarray): One-hot labels of shape (m, classes).
+        batch_size (int): Size of the batch used for mini-batch gradient
+            descent.
+        epochs (int): Number of passes through data for mini-batch gradient
+            descent.
+        verbose (bool): Determines if output should be printed during training.
+        shuffle (bool): Determines whether to shuffle the batches every epoch.
+
+    Returns:
+        keras.callbacks.History: The History object generated after training the
+        model.
+    """
+    history = network.fit(
+        data,
+        labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle
+    )
+    return history
