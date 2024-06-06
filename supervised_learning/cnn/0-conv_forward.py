83b85a5bafca1dcc83b5c42f92ec8debb2418782
dassantoss <santy.e@gmail.com>
2024-06-06 13:17:40 -0600

    Fix output

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -11,7 +11,7 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
 
     Args:
         A_prev (numpy.ndarray): of shape (m, h_prev, w_prev, c_prev)
-            ontaining the output of the previous layer
+            containing the output of the previous layer
             m is the number of examples
             h_prev is the height of the previous layer
             w_prev is the width of the previous layer
@@ -32,7 +32,7 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
             sh is the stride for the height
             sw is the stride for the width
 
-        Returns:
+    Returns:
         numpy.ndarray: Output of the convolutional layer.
     """
     m, h_prev, w_prev, c_prev = A_prev.shape
@@ -55,9 +55,15 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
 
     for i in range(output_h):
         for j in range(output_w):
-            region = A_prev_padded[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :]
             for k in range(c_new):
-                Z[:, i, j, k] = np.sum((region * W[:, :, :, k]),
+                vert_start = i * sh
+                vert_end = vert_start + kh
+                horiz_start = j * sw
+                horiz_end = horiz_start + kw
+                A_slice = A_prev_padded[:, vert_start:vert_end,
+                                        horiz_start:horiz_end, :]
+                Z[:, i, j, k] = np.sum(A_slice * W[:, :, :, k],
                                        axis=(1, 2, 3))
 
-    return activation(Z + b)
+    Z += b
+    return activation(Z)
