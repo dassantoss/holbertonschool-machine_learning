e731fbc733565275dc077957a82263103d4718dd
dassantoss <santy.e@gmail.com>
2024-07-21 14:14:21 -0600

    Implement Binomial class with initialization, PMF, and CDF methods

---
+++ math/probability/binomial.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+"""
+Module to represent a Binomial distribution.
+"""
+
+
+class Binomial:
+    """
+    Represents a binomial distribution.
+    """
+
+    def __init__(self, data=None, n=1, p=0.5):
+        """
+        Initializes the binomial distribution.
+
+        Parameters:
+        data (list): Data to be used to estimate the distribution.
+        n (int): The number of Bernoulli trials.
+        p (float): The probability of a “success”.
+
+        Sets the instance attributes n and p.
+        Saves n as an integer and p as a float.
+
+        If data is not given (i.e., None):
+            - Use the given n and p.
+            - If n is not a positive value, raise a ValueError with the message
+              "n must be a positive value".
+            - If p is not a valid probability, raise a ValueError with the
+                message "p must be greater than 0 and less than 1".
+
+        If data is given:
+            - Calculate n and p from data.
+            - Round n to the nearest integer (rounded, not casting).
+            - Calculate p first and then calculate n. Then recalculate p.
+            - If data is not a list, raise a TypeError with the message
+              "data must be a list".
+            - If data does not contain at least two data points, raise a
+              ValueError with the message "data must contain multiple values".
+        """
+        if data is None:
+            if n <= 0:
+                raise ValueError("n must be a positive value")
+            if not (0 < p < 1):
+                raise ValueError("p must be greater than 0 and less than 1")
+            self.n = int(n)
+            self.p = float(p)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+            mean = sum(data) / len(data)
+            variance = sum((x - mean) ** 2 for x in data) / len(data)
+            p_est = 1 - (variance / mean)
+            n_est = round(mean / p_est)
+            p_est = mean / n_est
+
+            self.n = n_est
+            self.p = p_est
+
+    def _factorial(self, n):
+        """
+        Calculates the factorial of a given number `n`.
+
+        Parameters:
+        n (int): The number to calculate the factorial of.
+
+        Returns:
+        int: The factorial of n.
+        """
+        if n == 0:
+            return 1
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
+
+    def pmf(self, k):
+        """
+        Calculates the value of the PMF for a given number of “successes”.
+
+        Parameters:
+        k (int): The number of “successes”.
+
+        Returns:
+        float: The PMF value for k.
+        If k is out of range (k < 0 or k > n), returns 0.
+        """
+        k = int(k)
+        if k < 0 or k > self.n:
+            return 0
+
+        n_fact = self._factorial(self.n)
+        k_fact = self._factorial(k)
+        n_k_fact = self._factorial(self.n - k)
+
+        nck = n_fact / (k_fact * n_k_fact)
+        return nck * (self.p ** k) * ((1 - self.p) ** (self.n - k))
+
+    def cdf(self, k):
+        """
+        Calculates the value of the CDF for a given number of “successes”.
+
+        Parameters:
+        k (int): The number of “successes”.
+
+        Returns:
+        float: The CDF value for k.
+        If k is out of range (k < 0 or k > n), returns 0.
+        """
+        k = int(k)
+        if k < 0 or k > self.n:
+            return 0
+
+        return sum(self.pmf(i) for i in range(k + 1))
