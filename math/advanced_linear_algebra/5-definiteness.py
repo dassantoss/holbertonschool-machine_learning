77c68cecc8653a8d19b6cbb3c484093374fe4441
dassantoss <santy.e@gmail.com>
2024-07-20 19:50:32 -0600

    Fix output

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -24,8 +24,11 @@ def definiteness(matrix):
     if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
 
-    if len(matrix.shape) != 2 \
-            or matrix.shape[0] != matrix.shape[1] or matrix.size == 0:
+    if len(matrix.shape) != 2 or \
+            matrix.shape[0] != matrix.shape[1] or matrix.size == 0:
+        return None
+
+    if not np.allclose(matrix, matrix.T):
         return None
 
     eigenvalues = np.linalg.eigvals(matrix)
