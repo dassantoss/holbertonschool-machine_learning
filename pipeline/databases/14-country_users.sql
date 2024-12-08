2c8f87e715518540c8f0600e9b7891ab864fa862
dassantoss <santy.e@gmail.com>
2024-12-07 23:28:22 -0600

    Add SQL script to create a `users` table with attributes for id, email, name, and country enumeration

---
+++ pipeline/databases/14-country_users.sql
@@ -0,0 +1,7 @@
+-- Creates a table `users` with specified attributes and enumeration for `country`
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
