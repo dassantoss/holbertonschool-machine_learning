9735b174de6dbc4ee6f6381ad47d4a74ce2a955c
dassantoss <santy.e@gmail.com>
2024-12-07 23:10:12 -0600

    Add SQL script to count shows by genre, ordered by number of shows

---
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- Lists all genres and the number of shows linked to each genre
+-- Ordered by number of shows in descending order
+SELECT tv_genres.name AS genre, 
+       COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.id
+HAVING number_of_shows > 0
+ORDER BY number_of_shows DESC;
