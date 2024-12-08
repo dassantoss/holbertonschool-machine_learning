684d519a3ac0678ddf06a42eaa1fb9d304e2932d
dassantoss <santy.e@gmail.com>
2024-12-08 09:31:14 -0600

    Add SQL scripts to initialize the `users` table, perform updates on user data, and create a trigger to reset the `valid_email` status when the email is changed.

---
+++ pipeline/databases/18-valid_email.sql
@@ -0,0 +1,13 @@
+-- Creates a trigger to reset valid_email when email is changed
+DELIMITER $$
+
+CREATE TRIGGER reset_valid_email_on_change
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END$$
+
+DELIMITER ;
